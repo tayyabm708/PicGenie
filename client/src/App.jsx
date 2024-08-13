f7b297adf93334ebd780461a4855a6390036ae30
Muhammad Tayyab <tayyabm708@gmail.com>
2024-08-13 14:27:29 +0500

    installed react-router-dom and create HOme and Create Post pages and added navigation using Link component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div className='text-red-700 justify-center items-center flex flex-col'>
-      <h1>APP</h1>
-      <h1>nodnco</h1>
-    </div>
-  )
+    <BrowserRouter>
+     <Header />
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+     </Routes>
+    </BrowserRouter>
+  );
 }
