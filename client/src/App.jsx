0b7c71794076488065162199435215559df0e212
Muhammad Tayyab <tayyabm708@gmail.com>
2024-08-13 13:59:46 +0500

    Install react js and tailwind css and create first template

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='text-red-700 justify-center items-center flex flex-col'>
+      <h1>APP</h1>
+      <h1>nodnco</h1>
+    </div>
+  )
+}
