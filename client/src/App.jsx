8b3018cac0b542689fc58e73b64f0b786ee83a72
Muhammad Tayyab <tayyabm708@gmail.com>
2024-08-13 15:44:21 +0500

    create home page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo } from "./assets";
-import Home from "./pages/Home";
-import CreatePost from "./pages/CreatePost";
+import {Home, CreatePost } from "./pages"
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
      <Header />
+     
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Loader, Card, FormField } from "../components";
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+  );
+};
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState(null);
+  const [searchText, setsearchText] = useState("");
   return (
-    <div>Home</div>
-  )
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-black text-[32px]">
+          The Community Showcase
+        </h1>
+        <p className="mt-2 text-[#666e75] font-medium max-w-screen-xs">
+          Transform your words into vivid images with PicGenie. Unleash your
+          creativity and see your ideas come to life through AI-powered
+          visualizations{" "}
+        </p>
+
+        <div className="mt-16 ">
+          <FormField />
+        </div>
+
+        <div className="mt-10"></div>
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className="text-[#666e75] text-xl mb-3">
+                Showing results for{" "}
+                <span className="text-[3222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards data={[]} title="No search results found" />
+              ) : (
+                <RenderCards data={[]} title="No posts found" />
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
 }
