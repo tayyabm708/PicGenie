679f0dc71aca9446432f86ce2ea652c74811dbb5
Muhammad Tayyab <tayyabm708@gmail.com>
2024-08-23 23:14:13 +0500

    add prereqs for deploying

--- server/index.js
+++ server/index.js
@@ -2,16 +2,24 @@ import express from "express";
 import * as dotenv from "dotenv";
 import connectDB from "./mongodb/connect.js";
 import cors from "cors";
-import postRoutes from "./routes/postRoute.js"
-import dalleRoutes from "./routes/dalleRoute.js"
+import postRoutes from "./routes/postRoute.js";
+import dalleRoutes from "./routes/dalleRoute.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.get("/", async (req, res) => {
   res.send("Hello from PicGenie");
@@ -25,7 +33,6 @@ const startServer = async () => {
     });
   } catch (error) {
     console.log(error);
-    
   }
 };
 
